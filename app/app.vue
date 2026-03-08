e3c3842a838bbbdbceccfdad955021bdf7eba2f5
Andreas Morasch <a.morasch@basecom.de>
2026-03-08 14:06:05 +0100

    add vercel speed insights

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,7 @@
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
 <template>
+  <SpeedInsights/>
   <div id="app-root">
     <h1>Welcome to CrossHolder AI</h1>
     <h2>Currently Building... 🚧🛠️</h2>
